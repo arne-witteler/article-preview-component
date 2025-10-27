1e9b814582517bd06911a2e4fd94ad4d92739a92
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2025-10-27 10:57:53 +0100

    fix: update share toggle functionality to show menu correctly

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-document.querySelector('.share__toggle').addEventListener('click', function() {
-  document.querySelector('.share__menu').classList.toggle('share__menu--hidden');
+document.querySelector('.share__toggle').addEventListener('click', () => {
+  document.querySelector('.share__menu').classList.toggle('share__menu--visible');
 });
