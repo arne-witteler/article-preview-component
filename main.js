686d8af4c7455d76d4abfa8b3f170e99e0827a78
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2025-11-05 12:20:39 +0100

    initial commit

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 document.querySelector('.share__toggle').addEventListener('click', () => {
   document.querySelector('.share__menu').classList.toggle('share__menu--visible');
+  document.querySelector('.share__toggle').classList.toggle('active');
 });
